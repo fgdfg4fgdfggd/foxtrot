d8cdb0251ae1f6c9607e5e25a32f067b8c67d86c
fgdfg4fgdfggd <50755286+fgdfg4fgdfggd@users.noreply.github.com>
2020-01-16 21:05:55 -0600

    Update discord.js

--- classes/discord.js
+++ classes/discord.js
@@ -7,16 +7,16 @@ class Discord {
   sendAlert (info, deranked) {
     const log = info.first
     const sendData = {
-      username: 'Polaris Group Defender',
-      avatar_url: 'https://i.imgur.com/fXBLyRU.png',
-      content: '@everyone ALERT: Threat detected',
+      username: 'Project_Foxtrot',
+      avatar_url: 'https://cdn.discordapp.com/attachments/607243101391552512/667561761586544700/phantomlogo.png',
+      content: '@everyone ALERT: Group Threat detected',
       embeds: [
         {
-          title: `Possible admin abuse detected`,
+          title: `Possible Group Admin Abuse Detected`,
           description: `**User**: [${log.username}](${`https://www.roblox.com/users/${log.userId}/profile`}) (${log.userId}) \n**Rank**: \`${log.rankName} (${log.rank})\`\n**Number of actions**: ${info.no}\n${deranked ? 'User de-ranked.' : 'Failed to de-rank user!'}\n\n**DO NOT IGNORE THIS ALERT**`,
-          color: 11730954,
+          color: 16711935,
           url: `https://www.roblox.com/groups/${this.groupId}/-`,
-          footer: { text: "Powered by Polaris Group Defender" }
+          footer: { text: "Powered by RCDForum" }
         }
       ],
       tts: true
